4f03a8a4766398a86670511f7e707456729b4ec8
Tobias Hildebrandt <tobiash@tobiash.dev>
2023-01-09 01:16:37 -0500

    simplify cookie ergonomics
    set expiry and samesite cookie settings

--- backend/src/cookies.rs
+++ backend/src/cookies.rs
@@ -1,45 +1,45 @@
 use axum_extra::extract::{
-    cookie::{Cookie, Expiration},
+    cookie::{Cookie, Expiration, SameSite},
     CookieJar, PrivateCookieJar,
 };
-use common::{USERNAME_ID_COOKIE, USER_ID_COOKIE};
+use common::USER_ID_COOKIE;
 use time::OffsetDateTime;
 
 use crate::db::ExistingUser;
 
+const COOKIE_DURATION: time::Duration = time::Duration::WEEK;
+
 impl ExistingUser {
-    pub fn cookies(
-        &self,
-        private: PrivateCookieJar,
-        regular: CookieJar,
-    ) -> (PrivateCookieJar, CookieJar) {
-        // TODO: harden cookies
+    pub fn cookies(&self, private: PrivateCookieJar) -> PrivateCookieJar {
         // user id cookie
         let mut user_id_cookie = Cookie::new(USER_ID_COOKIE, self.id.to_string());
+
+        // set path
         user_id_cookie.set_path("/"); // matches all subpaths
 
-        // new private cookie jar
+        // set expire
+        let mut expiration = OffsetDateTime::now_utc();
+        expiration += COOKIE_DURATION;
+        user_id_cookie.set_expires(Expiration::from(Some(expiration)));
+
+        // set same site security
+        user_id_cookie.set_same_site(SameSite::Strict);
+
+        // new private cookie jar, shadow old one
         let private = private.add(user_id_cookie);
 
-        return (private, regular);
+        return private;
     }
 }
 
-pub fn wipe_cookies(
-    private: PrivateCookieJar,
-    regular: CookieJar,
-) -> (PrivateCookieJar, CookieJar) {
-    // remove old cookies
-    let private = private.remove(Cookie::named(USER_ID_COOKIE));
-    let regular = regular.remove(Cookie::named(USERNAME_ID_COOKIE));
-
-    // add regular blank cookies that have already expired
+pub fn get_wiped_cookie_jar() -> CookieJar {
+    let regular = CookieJar::new();
+    // add regular blank cookie that have already expired
     let mut user_id = Cookie::new(USER_ID_COOKIE, "");
     user_id.set_path("/"); // matches all subpaths
     user_id.set_expires(Expiration::from(OffsetDateTime::UNIX_EPOCH));
 
     let regular = regular.add(user_id);
-    // let regular = regular.add(username);
 
-    return (private, regular);
+    return regular;
 }
--- backend/src/endpoints/auth.rs
+++ backend/src/endpoints/auth.rs
@@ -4,10 +4,9 @@ use common::{LoginRequest, LoginSuccess, WhoAmIResponse, USER_ID_COOKIE};
 use hyper::StatusCode;
 use tracing::{error, info, instrument, warn};
 
-use crate::{cookies::wipe_cookies, db, BackendState};
+use crate::{cookies::get_wiped_cookie_jar, db, BackendState};
 
-// TODO: de-duplicate cookie code with a new struct that contains both private and regular jars
-// TODO: format and log relevant cookies
+// TODO: add session tracking to DB?
 #[axum::debug_handler]
 #[instrument(skip_all, fields(attempted))]
 pub async fn login(
@@ -16,46 +15,49 @@ pub async fn login(
         connection_pool,
     }): State<BackendState>,
     private_cookies: PrivateCookieJar,
-    regular_cookies: CookieJar,
     login_req: Json<LoginRequest>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, (hyper::StatusCode, CookieJar)> {
     tracing::Span::current().record("attempted", &login_req.username);
 
-    // mutable
-    let (mut private_cookies, mut regular_cookies) = (private_cookies, regular_cookies);
+    // closure to allow ? operator
+    // TODO: convert to a try block once the feature is stabilized in rust
+    let user = match (|| {
+        let conn = &mut connection_pool
+            .get()
+            .map_err(|e| hyper::StatusCode::from(e))?;
+        let user = db::attempt_login(conn, &login_req.username, &login_req.password)
+            .map_err(|e| hyper::StatusCode::from(e))?;
 
-    // wipe no matter what
-    (private_cookies, regular_cookies) = wipe_cookies(private_cookies, regular_cookies);
+        Ok(user)
+    })() {
+        Ok(u) => u,
+        Err(status) => {
+            return Err((status, get_wiped_cookie_jar()));
+        }
+    };
 
-    let conn = &mut connection_pool.get()?;
-    let user = db::attempt_login(conn, &login_req.username, &login_req.password)?;
-    (private_cookies, regular_cookies) = user.cookies(private_cookies, regular_cookies);
+    // success, add real cookies
+    let private_cookies = user.cookies(private_cookies);
 
     info!(id = user.id, username = user.username);
 
     return Ok((
-        StatusCode::OK,
+        hyper::StatusCode::OK,
         private_cookies,
-        regular_cookies,
         Json(LoginSuccess {
             username: user.username,
         }),
     ));
 }
 
-// TODO: remove session from database?
-// TODO: better error logging
+// TODO: fail if not logged in?
 #[axum::debug_handler]
-#[instrument(skip(private_cookies, regular_cookies))]
-pub async fn logout(
-    State(_): State<BackendState>, // for cookie jar key
-    private_cookies: PrivateCookieJar,
-    regular_cookies: CookieJar,
-) -> impl IntoResponse {
+#[instrument]
+pub async fn logout() -> impl IntoResponse {
     // for now, just tell client to wipe all cookies
-    let (private_cookies, regular_cookies) = wipe_cookies(private_cookies, regular_cookies);
+    let wiped = get_wiped_cookie_jar();
     info!("logged out");
-    return (StatusCode::OK, private_cookies, regular_cookies);
+    return (StatusCode::OK, wiped);
 }
 
 #[axum::debug_handler]
@@ -67,6 +69,7 @@ pub async fn whoami(
     }): State<BackendState>,
     private_cookies: PrivateCookieJar,
 ) -> Result<impl IntoResponse, StatusCode> {
+    // check for user id cookie
     let user_id = match private_cookies.get(USER_ID_COOKIE) {
         Some(c) => c.value().to_string(),
         None => {
@@ -75,6 +78,7 @@ pub async fn whoami(
         }
     };
 
+    // parse user id cookie
     let user_id = match user_id.parse::<i32>() {
         Ok(u) => u,
         Err(_e) => {
@@ -83,6 +87,7 @@ pub async fn whoami(
         }
     };
 
+    // get database connection
     let mut conn = match connection_pool.get() {
         Ok(c) => c,
         Err(e) => {
@@ -91,6 +96,7 @@ pub async fn whoami(
         }
     };
 
+    // get user from DB
     let u = db::get_user_by_id(&mut conn, user_id)?;
 
     info!(u = u.username);
